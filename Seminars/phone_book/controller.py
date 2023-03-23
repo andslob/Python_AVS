f1f7d34a2c105a0d32dba7314550b1e1dcd47483
Andrey Slobodchikov <andslob@inbox.ru>
2023-03-23 20:28:02 +0500

    Готово

--- Seminars/phone_book/controller.py
+++ Seminars/phone_book/controller.py
@@ -1,6 +1,34 @@
 import phone_book
+import view
 
 pb = phone_book.PhoneBook('phone_db.txt')
 
-print(pb)
-print(pb.phone_list[1])
+while True:
+    print(pb.main_menu())
+    choice = int(input('Выберите пункт меню: '))
+    match choice:
+        case 1:
+            view.print_pb(pb)
+        case 2:
+            name = input('Введите имя: ')
+            phone = input('Введите телефон: ')
+            comment = input('Введите комментарий: ')
+            pb.new_contact(name, phone, comment)
+        case 3:
+            word = input('введите поисковый запрос: ')
+            view.print_pb(pb.search(word))
+        case 4:
+            view.print_pb(pb)
+            index = int(input('Введите индекс контакта, который будем изменять: '))
+            name = input('Введите имя (или Enter - оставить без изменений):')
+            phone = input('Введите телефон (или Enter - оставить без изменений):')
+            comment = input('Введите комментарий (или Enter - оставить без изменений):')
+            pb.change(index-1, name, phone, comment)
+        case 5:
+            view.print_pb(pb)
+            index = int(input('Введите индекс контакта, который хотите удалить: '))
+            pb.delete(index-1)
+        case 6:
+            pb.save()
+        case 7:
+            break
--- Seminars/phone_book/phone_book.py
+++ Seminars/phone_book/phone_book.py
@@ -29,8 +29,39 @@ class PhoneBook:
         with open(self.path, 'w', encoding="UTF-8") as file:
             file.write(data)
 
+    def main_menu(self):
+        return '''Главное меню
+        1. Показать все контакты
+        2. Создать контакт
+        3. Найти контакт
+        4. Изменить контакт
+        5. Удалить контакт
+        6. Сохранить изменения
+        7. Выход'''
+
+    def new_contact(self, name: str, phone: str | int, comment: str):
+        self.phone_list.append(Contact(name, phone, comment))
+
+    def search(self, find: str):
+        result = []
+        for contact in  self.phone_list:
+            if find in contact.to_str():
+                result.append(f'{contact}')
+        return '\n'.join(result)
+
+    def change(self, index: int, name: str, phone: str, comment: str):
+        name = name if name != '' else self.phone_list[index].name
+        phone = phone if phone != '' else self.phone_list[index].phone
+        comment = comment if comment != '' else self.phone_list[index].comment
+        self.phone_list[index] = Contact(name, phone, comment)
+
+    def delete(self, index: int):
+        self.phone_list.pop(index)
+
     def __str__(self):
         result = ''
+        i = 0
         for contact in self.phone_list:
-            result += f'{contact}\n'
+            i += 1
+            result += f'{i}. {contact}\n'
         return result[:-2]
